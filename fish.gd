4f9f4094679146b48272898bfcdd4c060c1b7fe1
noobspi <bernd@lingue.at>
2025-11-16 12:48:56 +0100

    add assets for creating a visual depth to the aquarium. Added  in the scene-tree some layers with rocks and gras within the  main-scene. fish can swin between those layers-of-depth

--- fish.gd
+++ fish.gd
@@ -5,16 +5,18 @@ enum FishType  {BLINKY, SCHOOLFISH, SUBMARINE}
 @export var anim_speed_scale:float = 1.0
 @export var fish_type:FishType = FishType.BLINKY
 
-var left_or_right:int
+var fsm__moving_left_or_right:int = 0 	# 0 = left, 1 = right
 var anim_sprite:AnimatedSprite2D
 
 
-func _ready() -> void:
-	left_or_right = 1	# 1= right / 0 = left
-	
+# Setup animated-sprites: which fish to display?
+func _setup_sprite() -> void:
+	# 1. Hide all sprites
 	for f in get_children():
-		f.visible = false
+		if f is AnimatedSprite2D:
+			f.visible = false
 	
+	# 2. Select the correct sprite
 	match fish_type:
 		FishType.BLINKY:
 			anim_sprite = $sprite_blinky
@@ -22,23 +24,31 @@ func _ready() -> void:
 			anim_sprite = $sprite_schoolfish
 		FishType.SUBMARINE:
 			anim_sprite = $sprite_submarine
-			
-	anim_sprite.speed_scale = anim_speed_scale
-	anim_sprite.play()
-	anim_sprite.visible = true
+	
+	# 3. Apply properties and show
+	if is_instance_valid(anim_sprite):
+		anim_sprite.speed_scale = anim_speed_scale
+		anim_sprite.play()
+		anim_sprite.visible = true
+
+func _ready() -> void:
+	fsm__moving_left_or_right = 1
+	_setup_sprite()
+	
+
 
 	
 func _process(delta: float) -> void:
 	# FSM
 	if anim_sprite.position.x >= 1500:
-		left_or_right = 0
+		fsm__moving_left_or_right = 0
 		anim_sprite.scale.x *= -1
 	if anim_sprite.position.x <= -100:
-		left_or_right = 1
+		fsm__moving_left_or_right = 1
 		anim_sprite.scale.x *= -1
 
 	# Movement
-	if left_or_right == 1:
+	if fsm__moving_left_or_right == 1:
 		anim_sprite.position.x += int(1 * anim_speed_scale)
 	else:
 		anim_sprite.position.x -= int(1 * anim_speed_scale)
--- main.gd
+++ main.gd
@@ -1,21 +1,22 @@
 extends Node2D
 
 @onready var left_or_right:int = 1	# 1= right / 0 = left
+@onready var testfish: Node2D = $sprite_fish1
 
 
 
 func _process(delta: float) -> void:
-	if $sprite_fish1.position.x >= 1000:
+	if testfish.position.x >= 1000:
 		left_or_right = 0
-		$sprite_fish1.scale.x *= -1
-	if $sprite_fish1.position.x <= 100:
+		testfish.scale.x *= -1
+	if testfish.position.x <= 100:
 		left_or_right = 1
-		$sprite_fish1.scale.x *= -1
+		testfish.scale.x *= -1
 
 	if left_or_right == 1:
-		$sprite_fish1.position.x += 1
+		testfish.position.x += 1
 	else:
-		$sprite_fish1.position.x -= 1
+		testfish.position.x -= 1
 
 
 	
