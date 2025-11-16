3a75e98676c0584075592698e695e79a20efbd13
noobspi <bernd@lingue.at>
2025-11-16 19:15:44 +0100

    organize aqaurium-deco  and fish in layers

--- main.gd
+++ main.gd
@@ -1,23 +1,9 @@
 extends Node2D
 
-@onready var left_or_right:int = 1	# 1= right / 0 = left
-@onready var testfish: Node2D = $sprite_fish1
-
-
 
 func _process(delta: float) -> void:
-	if testfish.position.x >= 1000:
-		left_or_right = 0
-		testfish.scale.x *= -1
-	if testfish.position.x <= 100:
-		left_or_right = 1
-		testfish.scale.x *= -1
-
-	if left_or_right == 1:
-		testfish.position.x += 1
-	else:
-		testfish.position.x -= 1
-
+	pass
+	
 
 	
 	
